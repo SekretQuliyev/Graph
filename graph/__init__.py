79f746dbe14afea955bfe4bce99f0a39158610a1
SekretQuliyev <77120705+SekretQuliyev@users.noreply.github.com>
2021-02-10 16:07:56 +0400

    Update __init__.py

--- graph/__init__.py
+++ graph/__init__.py
@@ -13,4 +13,4 @@ from userge.config import Config, get_version  # noqa
 from userge.core import (  # noqa
     Userge, filters, Message, get_collection, pool)
 
-userge = Userge()  # userge is the client name
+graph = Graph()  # userge is the client name
